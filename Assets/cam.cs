3eaeb53964201a467ed92aeb40eb5c05840e6e9e
Turki Asiri <Turki01156@gmail.com>
2024-03-03 15:07:03 +0300

    Assignmente Schemes K&M
    control Schemes for Keyboard & Mouse
    3rd person camera to utilize

--- Assets/cam.cs
+++ Assets/cam.cs
@@ -7,10 +7,22 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float rotationSpeed = 5f;
     public Vector3 offset;
+    public PlayerActions a_PlayerActions;
+    public Rigidbody Rigidbody;
+    [SerializeField]
+    private Transform playerTransform;
+    private Vector2 mouseInput;
 
     private float mouseX, mouseY;
 
     private bool isCursorVisible = true;
+
+    public float horizontalSensitivity = 5f;
+    private float rotationY = 0f;
+
+    public float minZoomDistance = 2f;
+    public float maxZoomDistance = 10f;
+    public float zoomSpeed = 0.25f;
 
     private void Update()
     {
@@ -34,10 +46,49 @@ public class CameraFollow : MonoBehaviour
             }
         }
     }
+
     private void LateUpdate()
     {
         Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0f);
         transform.position = target.position + rotation * offset;
         transform.LookAt(target);
+    }
+
+    public void Awake()
+    {
+        a_PlayerActions = new PlayerActions();
+        a_PlayerActions.Gameplay.ZoomCamera.performed += context => Zoom(context.ReadValue<float>());
+
+        if (playerTransform == null)
+        {
+            Debug.LogError("playerTransform is NULL for camera!");
+        }
+    }
+
+    public void Zoom(float zoomInput)
+    {
+        float zoomDistance = Mathf.Lerp(minZoomDistance, maxZoomDistance, zoomInput);
+        offset = offset.normalized * zoomDistance;
+        Debug.Log("Zoom Input: " + zoomInput);
+
+    }
+
+    public void FixedUpdate()
+    {
+        float rotationYDelta = horizontalSensitivity * mouseInput.x * Time.deltaTime;
+        mouseInput = Vector2.zero;
+        rotationY += rotationYDelta;
+        Vector3 playerRotation = new Vector3(0f, rotationY, 0f);
+        playerTransform.rotation = Quaternion.Euler(playerRotation);
+    }
+
+    public void OnEnable()
+    {
+        a_PlayerActions.Gameplay.Enable();
+    }
+
+    public void OnDisable()
+    {
+        a_PlayerActions.Gameplay.Disable();
     }
 }
--- Assets/cube_ move.cs
+++ Assets/cube_ move.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
     public float speed = 5f;
-
+    public PlayerActions a_PlayerActions;
+    public Rigidbody Rigidbody;
+    public Vector2 movement;
     private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -17,11 +20,44 @@ public class CubeMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
+
             Vector3 jumpForce = new Vector3(0f, 5f, 0f);
             GetComponent<Rigidbody>().AddForce(jumpForce, ForceMode.Impulse);
 
+        }     
+     
+
+    }
+    public void OnEnable()
+    {
+        a_PlayerActions.Gameplay.Enable();
+    }
+    public void OnDisable()
+    {
+
+
+        a_PlayerActions.Gameplay.Disable();
+
+    }
+    public void FixedUp()
+        {
+        movement = a_PlayerActions.Gameplay.Gameplay.ReadValue<Vector2>();
+        Rigidbody.velocity = new Vector3(movement.x * speed, Rigidbody.velocity.y, movement.y * speed);
+
         }
+    public void Awake()
+    {
+        a_PlayerActions = new PlayerActions();
+        Rigidbody = GetComponent<Rigidbody>();
+        if (Rigidbody== null)
+        {
+            Debug.LogError("rigidBody is NULL for player");
+        }
+
+
     }
 
+
 }
+
+
